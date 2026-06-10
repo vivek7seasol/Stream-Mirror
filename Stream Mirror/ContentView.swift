a611232825a9921cbbb5ae01cedf4313a39e20a5
Vivek Rakholiya <vivek.7seasol@gmail.com>
2026-06-10 11:41:56 +0530

    Initial Commit

---
+++ Stream Mirror/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stream Mirror
+//
+//  Created by Vivek Rakholiya on 10/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stream Mirror/Stream_MirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stream_MirrorApp.swift
+//  Stream Mirror
+//
+//  Created by Vivek Rakholiya on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Stream_MirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
